ff36b2b7a6715f1edba010191d0acefc75a79263
Un1c0re <sayapin.p.a02@gmail.com>
2025-08-12 21:16:15 +0500

    [~] настроил подключение к локальному хосту

--- GarageBooking/ClientApp/vite.config.ts
+++ GarageBooking/ClientApp/vite.config.ts
@@ -18,5 +18,12 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5096",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
